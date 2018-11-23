15ddee9c967bee3f513a5a225b70df5f90220539
zahmed4 <zahmed4@lsu.edu>
2018-11-23 02:08:51 -0800

    "made some changes, graph still doesnt work"

--- meerkatSim.py
+++ meerkatSim.py
@@ -21,17 +21,15 @@ class user_input(object):
 		numOff_normalLight = np.random.randint(2,4,1)
 		numOff_smallDark = np.random.randint(0,4,1)
 		numOff_normalDark = np.random.randint(0,3,1)
-		total_smallLight = (numOff_smallLight * smallLight)
-		total_normalLight = (numOff_normalLight * normalLight)
-		total_smallDark = (numOff_smallDark * smallDark)
-		total_normalDark = (numOff_normalDark * normalDark)
+		total_smallLightOff = (numOff_smallLight * smallLight)
+		total_normalLightOff = (numOff_normalLight * normalLight)
+		total_smallDarkOff = (numOff_smallDark * smallDark)
+		total_normalDarkOff = (numOff_normalDark * normalDark)
 
-		num = [startPop]*(numberOfGen+1)
-
-
-		pheno = [total_smallLight, total_normalLight, total_smallDark, total_normalDark]
+		pheno = [total_smallLightOff, total_normalLightOff, total_smallDarkOff, total_normalDarkOff]
 		adult_gen = [smallLight, normalLight, smallDark, normalDark]
 		total_gen = [smallLight, normalLight, smallDark, normalDark]
+		per_gen = [pheno, adult_gen]
 		a = 0
 		while a < len(pheno):
 			for i in range(numberOfGen):
@@ -39,25 +37,38 @@ class user_input(object):
 				numOff_normalLight = np.random.randint(2,4,1)
 				numOff_smallDark = np.random.randint(0,4,1)
 				numOff_normalDark = np.random.randint(0,3,1)
-				total_smallLight = (numOff_smallLight * smallLight)
-				total_normalLight = (numOff_normalLight * normalLight)
-				total_smallDark = (numOff_smallDark * smallDark)
-				total_normalDark = (numOff_normalDark * normalDark)
+				total_smallLightOff = (numOff_smallLight * smallLight)
+				total_normalLightOff = (numOff_normalLight * normalLight)
+				total_smallDarkOff = (numOff_smallDark * smallDark)
+				total_normalDarkOff = (numOff_normalDark * normalDark)
 
 				total_gen[a] = ((pheno[a] + adult_gen[a]) + total_gen[a])
 
 			a += 1
+
+#		for z in numberOfGen:
+#			per_gen = (pheno + adult_gen)
+#			per_gen[z+1] = per_gen[z] 
+
+
+		num_1 = [smallLight]*(numberOfGen+1)
+		num_2 = [normalLight]*(numberOfGen+1)
+		num_3 = [smallDark]*(numberOfGen+1)
+		num_4 = [normalDark]*(numberOfGen+1)
+
 		print("Total number of small size, light coat Meerkats: " + str(total_gen[0]))
 		print("Total number of normal size, light coat Meerkats: " + str(total_gen[1]))
 		print("Total number of small size, dark coat Meerkats: " + str(total_gen[2]))
 		print("Total number of normal size, dark coat Meerkats: " + str(total_gen[3]))
 		print(numberOfGen)
 		print(total_gen[0])
+#		print(per_gen[a])
 
-		plt.plot(range(numberOfGen+1), num,  color='red')
-		plt.plot(range(numberOfGen+1), num, color='blue')
-		plt.plot(range(numberOfGen+1), num, color='green')
-		plt.plot(range(numberOfGen+1), num, color='purple')
+
+		plt.plot(range(numberOfGen+1), num_1,  color='red')
+#		plt.plot(range(numberOfGen+1), num_2, color='blue')
+#		plt.plot(range(numberOfGen+1), num_3, color='green')
+#		plt.plot(range(numberOfGen+1), num_4, color='purple')
 
 
 # Labeling x and y axis and formatting plot.
